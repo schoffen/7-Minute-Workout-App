bccb8a0150e31d7edc18fd49f1e1c906ae456110
Felipe Schoffen <fschoffen@yahoo.com>
2022-09-14 14:22:27 -0300

    initial commit

---
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minuteworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this, "Clicked Start", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
