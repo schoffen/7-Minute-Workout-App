78fefc1373ad9a50ea3abac67666f5183aaeab65
Felipe Schoffen <fschoffen@yahoo.com>
2022-09-29 15:20:56 -0300

    add history activity

---
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a7minuteworkout.databinding.ActivityHistoryBinding
+
+class HistoryActivity : AppCompatActivity() {
+    private var binding : ActivityHistoryBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.tbHistory)
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.title = "HISTORY"
+        }
+        binding?.tbHistory?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
